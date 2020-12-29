7c133d020f3b4c2b265e79bcd34f71f167135c2c
Philippe Coloigner <philippe.coloigner@gmail.com>
2020-12-29 17:57:29 +0100

    Ajout entité Club

--- Application/Schema.sql
+++ Application/Schema.sql
@@ -1,1 +1,6 @@
 -- Your database schema. Use the Schema Designer at http://localhost:8001/ to add some tables.
+CREATE TABLE club (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    nom TEXT NOT NULL,
+    nom_entier TEXT NOT NULL
+);
--- Web/FrontController.hs
+++ Web/FrontController.hs
@@ -5,12 +5,14 @@ import Web.Controller.Prelude
 import Web.View.Layout (defaultLayout)
 
 -- Controller Imports
+import Web.Controller.Club
 import Web.Controller.Static
 
 instance FrontController WebApplication where
     controllers = 
         [ startPage WelcomeAction
         -- Generator Marker
+        , parseRoute @ClubController
         ]
 
 instance InitControllerContext WebApplication where
